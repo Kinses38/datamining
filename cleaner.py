3503cc00115247d2c1902b97c6c7b8fd8aef35c2
kinses38 <shaun.kinsella38@mail.dcu.ie2>
2018-10-30 19:23:56 +0000

    Updated with James's code

--- cleaner.py
+++ cleaner.py
@@ -1,10 +1,10 @@
-import datetime
-import json
-import os
-import time
-
-import dateutil.relativedelta
+# -*- coding: utf-8 -*-
 import pandas as pd
+import os
+import datetime
+import dateutil.relativedelta
+import json
+import time
 
 DROP_COLUMNS = ['blurb', 'converted_pledged_amount', 'currency', 'currency_symbol', 'currency_trailing_code',
                 'current_currency', 'disable_communication', 'friends', 'fx_rate', 'is_backing', 'is_starrable',
@@ -82,32 +82,36 @@ def create_lifetime_in_days(c):
     except Exception, err:
         print Exception, err
 
-
-def create_until_state_changed_in_days(c):
-    cols = ['state_changed_at', 'launched_at']
+def classify_created_at(c):
+    cols = ['created_at']
     name = c
-    print 'FUNC :- create_until_state_changed_in_days CSV:', name
+    print 'FUNC :- classify_created_at CSV:', name
     try:
         c = pd.read_csv(c, error_bad_lines=False)
-        c['until_state_changed_in_days'] = ''  # creating new column
-        for index, row in c.iterrows():  # iterate through rows
-            state_changed = datetime.datetime.fromtimestamp(row['state_changed_at'])  # grab state_changed_at timestamp
-            launched_at = datetime.datetime.fromtimestamp(row['launched_at'])  # grab launched_at timestamp
-            until_state_changed_in_days = dateutil.relativedelta.relativedelta(state_changed,
-                                                                               launched_at)  # subtract the two timestamps. Output is in years, months, days, minutes
-            # print until_state_changed_in_days
-            daysToAdd = 0
-            daysToAdd = until_state_changed_in_days.years * 365  # convert years to days
-            daysToAdd += until_state_changed_in_days.months * 30  # convert months to days
-            if until_state_changed_in_days.hours >= 12:
-                until_state_changed_in_days.days += 1
-            until_state_changed_in_days.days += daysToAdd
-            until_state_changed_in_days = until_state_changed_in_days.days
-            c.loc[c.index[index], 'until_state_changed_in_days'] = until_state_changed_in_days  # write to the cell
-        c.to_csv(name, index=False)
+        c['quarter_created_at'] = ''
+        c['year_created_at'] = ''
+        for index, row in c.iterrows():
+            created_at = datetime.datetime.fromtimestamp(row['created_at'])
+	    created_at = str(created_at).split("-")
+
+	    #Creating season quartile	
+	    if int(created_at[1]) < 4:
+            	c.loc[c.index[index], 'quarter_created_at'] = 1
+	    elif int(created_at[1]) < 7:
+            	c.loc[c.index[index], 'quarter_created_at'] = 2
+	    elif int(created_at[1]) < 10:
+            	c.loc[c.index[index], 'quarter_created_at'] = 3
+	    elif int(created_at[1]) < 13: 
+            	c.loc[c.index[index], 'quarter_created_at'] = 4
+	    else:
+            	c.loc[c.index[index], 'quarter_created_at'] = "ERROR!"
+		
+	    #fill year_created_at
+            c.loc[c.index[index], 'year_created_at'] = created_at[0]
+		 	
+    	c.to_csv(name, index=False)
     except Exception, err:
         print Exception, err
-
 
 def main():
     # Get the list of CSV's in the current directory
@@ -124,8 +128,8 @@ def main():
     for CSV in CSV_files:
         remove_columns(CSV)
         create_lifetime_in_days(CSV)
-        create_until_state_changed_in_days(CSV)
-        remove_columns(CSV, ['state_changed_at', 'launched_at', 'deadline'])
+	classify_created_at(CSV)
+        remove_columns(CSV, ['created_at', 'state_changed_at', 'launched_at', 'deadline'])
         parse_json_category(CSV, 'category', 'slug')
         parse_json_creator(CSV)
         print "Completed ", CSV
