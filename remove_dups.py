0371cdf8428122a0a10940f837925dc1343941c7
kinses38 <shaun.kinsella38@mail.dcu.ie2>
2018-11-21 12:56:40 +0000

    Separate dedup and trim

--- remove_dups.py
+++ remove_dups.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
 
-def remove_dups_and_trim(file_name):
+def remove_dups(file_name):
 
     df = pd.read_csv(file_name, error_bad_lines=False)
     df = df.drop_duplicates(subset='id', keep='first', inplace=False)
+    df.to_csv('Deduped_Master.csv', index=False)
+    print "Completed removing duplicates and trimmed on file: " + file_name
 
+
+def trim(file_name):
+    df = pd.read_csv(file_name, error_bad_lines=False)
     # for_pledged%
-    df = df.sort_values(by=['pledged %'])
+    # df = df.sort_values(by=['pledged %'])
 
-    #df = df.iloc[1000:]
-    #df = df.iloc[:-9000]
+    # df = df.iloc[1000:]
+    # df = df.iloc[:-9000]
 
     # for_goal
     # df = df.sort_values(by=['goal'])
@@ -21,9 +26,8 @@ def remove_dups_and_trim(file_name):
     # df = df.sort_values(by=['backers_count'])
     # df = df.iloc[3000:]
     # df = df.iloc[:-5000]  # see screenshots of 5-number for this category
-
-    df.to_csv('Deduped_Master.csv', index=False)
-    print "Completed removing duplicates and trimmed"
+    df.to_csv('Trimmed_Deduped_Master.csv', index=False)
+    print "Completed trimming on file: " + file_name
 
 
 def categorise_pledged(file_name):
@@ -37,8 +41,12 @@ def categorise_pledged(file_name):
     df.to_csv('Cat_Master.csv', index=False)
     print "Completed changing pledged to categories in file " + file_name
 
+
 def main():
-    remove_dups_and_trim("Master.csv")
+    remove_dups("Master.csv")
+    trim("Deduped_Master.csv")
+
+    # This is for non-trimmed.
     categorise_pledged("Deduped_Master.csv")
 
 
